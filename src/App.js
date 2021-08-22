26d6484c3899cb8e32238a9592141bea2b5fa4ad
Bikram <bikramjit7777@gmail.com>
2021-08-22 11:50:46 -0400

    Recipe app initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "248981c3";
+  const APP_KEY = "eddf5860e7fde782dde32a708d972192";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [query, setQuery] = useState("Coffee");
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe App</h1>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button">Search</button>
+      </form>
+      <div className="recipes">
+        {recipes.map((recipee) => (
+          <Recipe
+            title={recipee.recipe.label}
+            calories={recipee.recipe.calories}
+            image={recipee.recipe.image}
+            ingredients={recipee.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
